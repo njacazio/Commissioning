12302f1bd534dcb265c18552d5f27a932b5c6bdb
Roberto Preghenella <preghenella@bo.infn.it>
2020-08-04 19:30:39 +0200

    Scripts for TOF pp simulation/digitisation

---
+++ SIM/pp/simulation.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+### default number of events is 22765 to
+### generate 256 orbits at 1 MHz rate
+
+### 2 seconds/event (on R+Docker), 0.04 Mbytes/event
+### 12 hours total time, 960 Mbytes
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+[ -z "$1" ] && NEVENTS=22765 || NEVENTS=$1
+
+echo " --- running simulation: ${NEVENTS} events "
+
+o2-sim -n ${NEVENTS} -g pythia8 -m PIPE TOF
