4e1335e06bd8c63ce3cd130f1600011e8369c977
Roberto Preghenella <preghenella@bo.infn.it>
2020-10-14 15:16:08 +0200

    Diagnostic analysis can now print a summary to stdout

--- ANALYSIS/diagnosic/diagnostic_analysis.C
+++ ANALYSIS/diagnosic/diagnostic_analysis.C
@@ -1,7 +1,14 @@
 #include "TOFWorkflowUtils/CompressedAnalysis.h"
 #include <iostream>
 
+#define colorReset "\033[0m"
+#define colorRed "\033[1;31m"
+#define colorGreen "\033[1;32m"
+#define colorYellow "\033[1;33m"
+#define colorBlue "\033[1;34m"
+
 using namespace o2::tof::compressed;
+using namespace o2::tof::diagnostic;
 
 class DiagnosticsAnalysis : public o2::tof::CompressedAnalysis
 {
@@ -32,6 +39,8 @@ private:
 		      const Diagnostic_t* diagnostics,
                       const Error_t* errors) final;  
 
+  void printSummary();
+  
   std::string mFileName = "noise_analysis.root";
   int mDiagnostics[72][12][32]; // [crate][slot][bit]
 
@@ -63,9 +72,44 @@ DiagnosticsAnalysis::finalize()
     h.Write();
   }
   fout.Close();
+  printSummary();
   return true;
 }
+
+void DiagnosticsAnalysis::printSummary()
+{
+  for (int icrate = 0; icrate < 72; ++icrate) {
+    // check we got events in this crate
+    if (mDiagnostics[icrate][0][0] == 0) continue;    
+
+    for (int islot = 0; islot < 32; ++islot) {
+      // check we got events in this slot
+      if (mDiagnostics[icrate][islot][0] == 0) continue;
+
+      for (int ibit = 0; ibit < 32; ++ibit) {
+	// check we got events in this bit
+	if (mDiagnostics[icrate][islot][ibit] == 0) continue;
+
+	const char* bitname = nullptr;
+	auto color = colorBlue;
+	if (ibit >= 4) color = colorRed;
+	if (islot == 0) bitname = DRMDiagnosticName[ibit];
+	else if (islot == 1) bitname = LTMDiagnosticName[ibit];
+	else bitname = TRMDiagnosticName[ibit];
+	auto norm = (float)mDiagnostics[icrate][islot][0];
+	if (ibit == 0) norm = (float)mDiagnostics[icrate][0][0];
+	std::cout << color
+		  << " --- CRATE-" << setw(2) << setfill('0') << icrate << " SLOT-" << setw(2) << setfill('0') << islot + 1
+		  << ": " << bitname << " in " << mDiagnostics[icrate][islot][ibit] << " events ( "
+		  << 100. * (float)mDiagnostics[icrate][islot][ibit] / norm << " % )"
+		  << colorReset << std::endl;
+      
+      }
+    }
+  }
   
+}
+
 void DiagnosticsAnalysis::headerHandler(const CrateHeader_t* crateHeader,
 					 const CrateOrbit_t* crateOrbit)
 {
