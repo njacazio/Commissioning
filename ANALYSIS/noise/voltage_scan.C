78529c2a9ce29b63d44b796c116ab7bfab6884bc
Roberto Preghenella <preghenella@bo.infn.it>
2020-10-27 16:56:24 +0100

    Few changes to voltage_scan macro

--- ANALYSIS/noise/voltage_scan.C
+++ ANALYSIS/noise/voltage_scan.C
@@ -1,20 +1,26 @@
 #include "style.C"
-TString outputdir = "/home/neelima/Desktop/commisionning/20201021_noise_scan/results/";
+//TString outputdir = "/home/neelima/Desktop/commisionning/20201021_noise_scan/results/";
+TString outputdir = "/home/preghenella/cernbox/ALICE/TOF/Commissioning2020/20201021_noise_scan/results";
 const int nruns   = 30;
 int run[nruns]    = {42, 43, 44, 45, 46, 47, 48, 51, 52, 54, 55, 56, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 71, 72, 73, 74, 75, 76, 77, 78}; 
 int hv[nruns]     = {13000, 13000, 13000, 13000, 13000, 13000, 12750, 12750, 12750, 12750, 12750, 12750, 12500, 12500, 12500, 12500, 12500, 12500, 12250, 12250, 12250, 12250, 12250, 12250, 12000, 12000, 12000, 12000, 12000, 12000};
 int thr[nruns]    = {1000, 900, 800, 700, 600, 500, 1000, 900, 800, 700, 600, 500, 1000, 900, 800, 700, 600, 500, 1000, 900, 800, 700, 600, 500, 1000, 900, 800, 700, 600, 500};
 
 int normaliseTo = -1;
+bool fitPol2 = false;
 
 std::pair<double,double>
 rate_value(int run, int sector, int strip, int fea) 
 {
   auto fin = TFile::Open(Form("%s/noise_map.%d.root", outputdir.Data(), run));
   if (sector == -1) {
+    auto f = (TF1*)gROOT->GetFunction("gaus");
     auto hFeaNoiseDist = (TH1 *)fin->Get("hFeaNoiseDist");
-    auto rate = hFeaNoiseDist->GetMean();
-    auto ratee = hFeaNoiseDist->GetMeanError();
+    hFeaNoiseDist->Fit(f, "0q", "", 0., 1.0);
+    auto rate = f->GetParameter(1);
+    auto ratee = f->GetParError(1);
+    //    auto rate = hFeaNoiseDist->GetMean();
+    //    auto ratee = hFeaNoiseDist->GetMeanError();
     return {rate,ratee};  }
   auto hFeaStripNoise = (TH2 *)fin->Get("hFeaNoise");
   // fea = 3 - fea; // filling is 3,2,1,0 on histogram
@@ -52,19 +58,19 @@ voltage_scan(int sector = 4, int strip = 44, int fea = 2)
     }
   } 
 
-  //  auto f2d = new TF2("f2d", "[0] + ([1] * x + [2] * x * x + [3] * y + [4] * y * y)");
   auto f2d = new TF2("f2d", "[0] + [1] * (x - 13000.) + [2] * (x - 13000.) * (x - 13000.) + [3] * (y - 500.) + [4] * (y - 500.) * (y - 500.)");
   f2d->SetParameter(0, 0.5);
   if (normaliseTo != -1) f2d->SetParameter(0, 1.);
   f2d->FixParameter(2, 0.);
   f2d->FixParameter(4, 0.);
   fea_Noise_2D->Fit(f2d, "0");
-  /*  f2d->ReleaseParameter(2);
-  fea_Noise_2D->Fit(f2d, "0");
-  f2d->ReleaseParameter(4);
-  fea_Noise_2D->Fit(f2d, "0"); */
+  if (fitPol2) {
+    f2d->ReleaseParameter(2);
+    fea_Noise_2D->Fit(f2d, "0");
+    f2d->ReleaseParameter(4);
+    fea_Noise_2D->Fit(f2d, "0");
+  }
   
-  // auto f1d = new TF1("f1d", "[0] + ([1] * [5] + [2] * [5] * [5] + [3] * x + [4] * x * x)", 400., 1100.);
   auto f1d = new TF1("f1d", "[0] + [1] * ([5] - 13000.) + [2] * ([5] - 13000.) * ([5] - 13000.) + [3] * (x - 500.) + [4] * (x - 500.) * (x - 500.)", 400., 1100.);
   f1d->SetLineStyle(kDashed);
   for (int i = 0; i < 5; ++i)
