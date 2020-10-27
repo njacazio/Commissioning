79396dce710a28cc2bb68a8c7f21819d0510f3dc
NEELIMA AGRAWAL <neelima.agrawal@cern.ch>
2020-10-27 15:58:55 +0100

    updated macro to include normalisation

--- ANALYSIS/noise/voltage_scan.C
+++ ANALYSIS/noise/voltage_scan.C
@@ -5,6 +5,8 @@ int run[nruns]    = {42, 43, 44, 45, 46, 47, 48, 51, 52, 54, 55, 56, 59, 60, 61,
 int hv[nruns]     = {13000, 13000, 13000, 13000, 13000, 13000, 12750, 12750, 12750, 12750, 12750, 12750, 12500, 12500, 12500, 12500, 12500, 12500, 12250, 12250, 12250, 12250, 12250, 12250, 12000, 12000, 12000, 12000, 12000, 12000};
 int thr[nruns]    = {1000, 900, 800, 700, 600, 500, 1000, 900, 800, 700, 600, 500, 1000, 900, 800, 700, 600, 500, 1000, 900, 800, 700, 600, 500, 1000, 900, 800, 700, 600, 500};
 
+int normaliseTo = -1;
+
 std::pair<double,double>
 rate_value(int run, int sector, int strip, int fea) 
 {
@@ -22,12 +24,16 @@ rate_value(int run, int sector, int strip, int fea)
 }
 
 void
-voltage_scan(int sector = 4, int strip = 44, int fea = 1)
+voltage_scan(int sector = 4, int strip = 44, int fea = 2)
 {
   style();
   int color[5] = {kRed+1, kOrange-3, kYellow+2, kGreen+2, kAzure-3};
   TGraphErrors* fea_Noise[5];
   TGraph2DErrors* fea_Noise_2D = new TGraph2DErrors();
+
+  std::pair<double,double> normalise_rate = {1., 1.};
+  if (normaliseTo != -1) normalise_rate = rate_value(run[normaliseTo], sector, strip, fea);
+  std::cout << " normalise rate = " << normalise_rate.first << std::endl;
   for (int i = 0; i < 5; i++){
     fea_Noise[i] = new TGraphErrors();
     fea_Noise[i]->SetMarkerStyle(20); 
@@ -38,32 +44,36 @@ voltage_scan(int sector = 4, int strip = 44, int fea = 1)
     for (int j = 0; j < 6; j++){
       auto irun = 6*i+j;
       auto rate = rate_value(run[irun], sector, strip, fea);
-      fea_Noise[i]->SetPoint(j, thr[irun], rate.first);
-      fea_Noise[i]->SetPointError(j, 0., rate.second);
+      fea_Noise[i]->SetPoint(j, thr[irun], rate.first / normalise_rate.first);
+      fea_Noise[i]->SetPointError(j, 0., rate.second / normalise_rate.first);
 
-      fea_Noise_2D->SetPoint(irun, hv[irun], thr[irun], rate.first);
-      fea_Noise_2D->SetPointError(irun, 0., 0., rate.second);
+      fea_Noise_2D->SetPoint(irun, hv[irun], thr[irun], rate.first / normalise_rate.first );
+      fea_Noise_2D->SetPointError(irun, 0., 0., rate.second / normalise_rate.first);
     }
   } 
 
   auto f2d = new TF2("f2d", "[0] + ([1] * x + [2] * x * x + [3] * y + [4] * y * y)");
+  //  auto f2d = new TF2("f2d", "[0] + [1] * (x - 13000.) + [2] * (x - 13000.) * (x - 13000.) + [3] * (y - 500.) + [4] * (y - 500.) * (y - 500.)");
+  f2d->SetParameter(0, 0.5);
+  if (normaliseTo != -1) f2d->SetParameter(0, 1.);
   f2d->FixParameter(2, 0.);
   f2d->FixParameter(4, 0.);
   fea_Noise_2D->Fit(f2d, "0");
-  f2d->ReleaseParameter(2);
+  /*  f2d->ReleaseParameter(2);
   fea_Noise_2D->Fit(f2d, "0");
   f2d->ReleaseParameter(4);
-  fea_Noise_2D->Fit(f2d, "0");
-
+  fea_Noise_2D->Fit(f2d, "0"); */
   
-  auto f1d = new TF1("f1d", "[0] + ([1] * [5] + [2] * [5] * [5] + [3] * x + [4] * x * x)", 400., 1100.);
+  // auto f1d = new TF1("f1d", "[0] + ([1] * [5] + [2] * [5] * [5] + [3] * x + [4] * x * x)", 400., 1100.);
+  auto f1d = new TF1("f1d", "[0] + [1] * ([5] - 13000.) + [2] * ([5] - 13000.) * ([5] - 13000.) + [3] * (x - 500.) + [4] * (x - 500.) * (x - 500.)", 400., 1100.);
   f1d->SetLineStyle(kDashed);
-  for (int i = 0; i < 5; ++i)
+  for (int i = 1; i < 5; ++i)
     f1d->SetParameter(i, f2d->GetParameter(i));
   
   auto c = new TCanvas("c", "c", 800, 800);
- 
-  c->DrawFrame(400., 0., 1100., 1., ";THR (mV);noise rate (Hz/channel)");
+  
+  if (normaliseTo == -1) c->DrawFrame(400., 0., 1100., 1., ";THR (mV);noise rate (Hz/channel)");
+  else c->DrawFrame(400., 0., 1100., 2., ";THR (mV);normalised noise rate");
   TLegend *leg = new TLegend(0.22, 0.65, 0.45, 0.9);
   leg->SetTextSize(0.04);
   leg->SetBorderSize(0);
