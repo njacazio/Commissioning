8f2b9725dd8cd731fc80581411236ee7177db2b9
Roberto Preghenella <preghenella@bo.infn.it>
2020-11-06 15:13:15 +0100

    Temporary update of script for reco+QC on FLP

--- DataDistribution/startRecoQC.sh
+++ DataDistribution/startRecoQC.sh
@@ -15,6 +15,15 @@ VERBOSE=""
 
 QCJSONRAW="${QUALITYCONTROL_ROOT}/etc/tofcompressed.json"
 QCJSONDIG="${QUALITYCONTROL_ROOT}/etc/tof.json"
+#QCJSONFUL="${QUALITYCONTROL_ROOT}/etc/toffull.json"
+QCJSONFUL="/home/flp/o2/QualityControl/Modules/TOF/toffull.json"
+QCJSON=${QCJSONFUL}
+
+#RECOWFLOWOPT="--input-type raw --output-type none --row-filter --mask-noise --noise-counts 10"
+RECOWFLOWOPT="--input-type raw --output-type none"
+
+#DIGIWRITEOPT="--ntf 2 --write-decoding-errors"
+DIGIWRITEOPT="--ntf 4096"
 
 o2-dpl-raw-proxy -b --session default \
     --dataspec "$PROXY_SPEC" \
@@ -23,11 +32,10 @@ o2-dpl-raw-proxy -b --session default \
     --tof-compressor-rdh-version 6 \
     --tof-compressor-config "$COMPR_CONF" \
     $VERBOSE \
-    | o2-tof-reco-workflow -b --session default \
-    --input-type raw \
-    --output-type digits \
+    | o2-tof-reco-workflow -b --session default ${RECOWFLOWOPT} \
+    | o2-tof-digit-writer-workflow -b --session default ${DIGIWRITEOPT} \
     | o2-qc -b --session default \
-    --config json://${QCJSONDIG} \
+    --config json://${QCJSON} \
     | o2-tof-compressed-inspector -b --session default \
     --tof-compressed-inspector-rdh-version 6 \
     --tof-compressed-inspector-filename inspector.root
