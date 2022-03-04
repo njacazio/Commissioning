37151ab78fabdc530f9090f67b13d651c6c6284f
Nicolò Jacazio <njacazio@users.noreply.github.com>
2022-03-04 17:11:02 +0100

    Extend SWAN init (#41)

--- QC/analysis/notebooks/swano2init.sh
+++ QC/analysis/notebooks/swano2init.sh
@@ -1,7 +1,17 @@
 #Initializer script for the O2 in swan
 
+if [[ -n "${1}" ]]; then
+    echo "Downloading SWAN resources"
+    wget https://raw.githubusercontent.com/alicetof/Commissioning/master/QC/analysis/notebooks/draw.ipynb
+    wget https://raw.githubusercontent.com/alicetof/Commissioning/master/QC/analysis/common.py
+    wget https://raw.githubusercontent.com/alicetof/Commissioning/master/QC/analysis/plotting.py
+    wget https://raw.githubusercontent.com/alicetof/Commissioning/master/QC/analysis/utilities/fetch_output.py
+    return
+fi
+
 export aTODAY=$(date -d "yesterday 13:00" "+%Y%m%d")
 version=VO_ALICE@O2::nightly-${aTODAY}-1
-eval `/cvmfs/alice.cern.ch/bin/alienv printenv $version`
-export CPPYY_BACKEND_LIBRARY=/cvmfs/alice.cern.ch/el7-x86_64/Packages/ROOT/v6-24-06-62/lib/libcppyy_backend3_6
+echo "Entering $version"
+eval $(/cvmfs/alice.cern.ch/bin/alienv printenv $version)
+export CPPYY_BACKEND_LIBRARY=${ROOTSYS}/lib/libcppyy_backend3_6
 export PYTHONPATH=${HOME}.local:$PYTHONPATH
